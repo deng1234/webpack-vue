34f65fb2ef300a3467223c6f3bfd9e4e0509936c
deng1234 <d13588345401@163.com>
2017-12-01 10:06:50 +0800

    update DB.php

--- server/DB.php
+++ server/DB.php
@@ -6,7 +6,7 @@
 	$host='localhost'; //数据库主机名
 	$dbName='Exam';    //使用的数据库
 	$user='root';      //数据库连接用户名
-	$pass='BoYa123.';      //对应的密码
+	$pass='root.';      //对应的密码
 	$dsn="$dbms:host=$host;dbname=$dbName";
 	try {
 		//初始化一个PDO对象
